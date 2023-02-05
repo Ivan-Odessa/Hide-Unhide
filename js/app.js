5dc60fc2ca6e668a6cc6b1aea4d4f0508dc46308
Ivan-Odessa <74266710+Ivan-Odessa@users.noreply.github.com>
2023-02-05 18:13:29 +0200

    Added Hide-Unhide blocks

---
+++ js/app.js
@@ -0,0 +1,33 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const btn = document.querySelector("#example__btn");
+    const script_text = document.querySelector("#example__text");
+    btn.addEventListener("click", (function() {
+        script_text.classList.toggle("example__text--hidden");
+        if (script_text.classList.contains("example__text--hidden")) btn.textContent = "Open block"; else btn.textContent = "Сlose block";
+    }));
+    window["FLS"] = true;
+    isWebp();
+})();
